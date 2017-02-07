844b670b9178b6711200ddaf617ec2472dbeac96
root <root@p404768.kvmvps>
2017-02-07 20:04:42 +0300

    pre deploy

--- flask-shop/fabfile.py
+++ flask-shop/fabfile.py
@@ -8,7 +8,7 @@ host_string = config.HOST_STRING
 
 def deploy():
     env.host_string = config.HOST_STRING
-    with cd('/var/www/flask-shop'):
+    with cd('/var/www/flask-shop/flask-shop'):
         with shell_env(MODE='PRODUCTION'):
             run('git reset --hard HEAD')
             run('git pull')
